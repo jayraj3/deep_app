57e72afeb15c67e605bdddee24956b3df2fd1048
jayraj  v parmar <jayrajp846@gmail.com>
2020-10-11 19:36:25 +0200

    modify path

--- deep_app/urls.py
+++ deep_app/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('deep/', include('deep.urls')),
+    path('', include('deep.urls')),
     path('admin/', admin.site.urls),
 ]
